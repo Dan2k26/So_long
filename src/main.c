2ee2ad8ee90ca7b16c7134b51e2c0d51148b9586
Dan <63083430+danlerma@users.noreply.github.com>
2022-10-03 19:49:15 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,17 +12,11 @@
 
 #include<so_long.h>
 
-void	check_leaks(void)
-{
-	system("leaks -q so_long");
-}
-
 int	main(int argc, char **argv)
 {
 	int		fd;
 	t_map	map;
 
-	atexit(check_leaks);
 	if (argc == 2)
 	{
 		fd = check_argument(argv[1]);
